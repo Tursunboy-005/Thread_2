de15d39f3c83a182c536b83827694a9428d85e06
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-23 11:14:56 +0500

    improve

--- Thread_2/Program.cs
+++ Thread_2/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Thread_2
+﻿using System.Diagnostics;
+
+namespace Thread_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stopwatch sw = new Stopwatch();
+            Thread thread = new Thread(() => Print());
+            Thread thread1 = new Thread(() => Print());
+            Thread thread2 = new Thread(() => Print());
+            Thread thread3 = new Thread(() => Print());
+            Thread thread4 = new Thread(() => Print());
+            thread.Name = "Test        ";
+            thread1.Name = "Test  1    ";
+            thread2.Name = "Test  2    ";
+            thread3.Name = "Test  3    ";
+            thread4.Name = "Test  4    ";
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+
+            sw.Stop();
+
+            Console.WriteLine(sw);
         }
+
+        public static void Print()
+        {
+            
+            for (double i = 0; i < 100000; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name +i);
+            }
+            
+        }
+
     }
 }
