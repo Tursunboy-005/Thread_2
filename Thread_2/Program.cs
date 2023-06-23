f1dbcb7c22595dd859982c309ee92b922bc22ef1
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-23 11:29:36 +0500

    locker instrument

--- Thread_2/Program.cs
+++ Thread_2/Program.cs
@@ -4,36 +4,45 @@ namespace Thread_2
 {
     internal class Program
     {
+        static object locker = new object();
         static void Main(string[] args)
         {
-           
+            #region thread
+
             Thread thread = new Thread(() => Print());
-            Thread thread1 = new Thread(() => Print());
-            Thread thread2 = new Thread(() => Print());
-            Thread thread3 = new Thread(() => Print());
-            Thread thread4 = new Thread(() => Print());
             thread.Name = "Test        ";
+
+            Thread thread1 = new Thread(() => Print());
             thread1.Name = "Test  1    ";
+
+            Thread thread2 = new Thread(() => Print());
             thread2.Name = "Test  2    ";
+
+            Thread thread3 = new Thread(() => Print());
             thread3.Name = "Test  3    ";
+
+            Thread thread4 = new Thread(() => Print());
             thread4.Name = "Test  4    ";
+
             thread1.Start();
             thread2.Start();
             thread3.Start();
             thread4.Start();
 
-            
+            #endregion
+
+
         }
 
         public static void Print()
         {
-            
-            for (double i = 0; i < 100000; i++)
+            lock(locker)
             {
-                Console.WriteLine(Thread.CurrentThread.Name + i);
+                for (double i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + i);
+                }
             }
-            
         }
-
     }
 }
